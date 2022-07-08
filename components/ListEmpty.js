9dc866a65ac4d190dd1b130cc350924fddd63b71
graphicalevolution <graphicalevolution@gmail.com>
2022-07-08 23:45:21 +1000

    Added ListEmpty component

---
+++ components/ListEmpty.js
@@ -0,0 +1,10 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+export function ListEmpty ( props ) {
+    return  (
+        <View>  
+            <Text>Empty List. Enter task in the box above!</Text>
+        </View>
+    )
+
+}
